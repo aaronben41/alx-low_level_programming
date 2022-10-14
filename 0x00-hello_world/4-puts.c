a2fac419c0409c54cf6cdc0f2365a053b1aad50c
Aaron <aaronben41@gmail.com>
2022-10-14 05:40:39 -0700

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ *main - Entry point
+ *
+ *
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *
+ */
+
+
+int main(void)
+  
+  
+{
+  
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+
+  return (0);
+  
+  
+}
